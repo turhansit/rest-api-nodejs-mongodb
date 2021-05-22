70521266b1440390fad71e0ee0d285cf326ed139
Turhan Şit <turhansitt@hotmail.com>
2021-05-22 22:55:43 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const postRoute = require('./routes/post');
+
+require('dotenv/config');
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/posts', postRoute)
+
+// Connect to DB
+mongoose.connect(process.env.db_connect, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then((result) => console.log('connect to db'))
+  .catch((err) => console.log(err));
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello MongoDB and Node.js Worlds');
+});
+
+// Listening
+app.listen(3000);
---
+++ routes/post.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const Post = require('../models/post');
+
+const router = express.Router();
+
+router.get('/get-data', async (request, response) => {
+    try {
+        const post = await Post.find();
+        response.json(post);
+    } catch (err) {
+        response.json({
+            mesaage: err
+        });
+    }
+});
+
+router.post('/post-data', (request, response) => {
+    const post = new Post({
+        title: request.body.title,
+        description: request.body.description,
+        short: request.body.short
+    });
+    post.save()
+        .then(data => {
+            response.json(data);
+        })
+        .catch(err => {
+            response.json({
+                mesaage: err
+            });
+        });
+});
+
+router.get('/search-data/:Id', async (request, response) => {
+    try {
+        const post = await Post.findById(request.params.Id);
+        response.json(post);
+    } catch (err) {
+        response.json({
+            mesaage: err
+        });
+    }
+});
+
+router.get('/search-data-with-request', async (request, response) => {
+    try {
+        const post = await Post.findById(request.body.Id);
+        response.json(post);
+    } catch (err) {
+        response.json({
+            mesaage: err
+        });
+    }
+});
+
+router.patch('/update-data/:Id', async (request, response) => {
+    try {
+        const updatedPost = await Post.updateOne({
+            _id: request.params.Id
+        }, {
+            $set: { title: request.body.title }
+        }, {
+            $set: { description: request.body.description }
+        }, {
+            $set: { short: request.body.short }
+        });
+        response.json(updatedPost);
+    } catch (err) {
+        response.json({
+            mesaage: err
+        });
+    }
+});
+
+router.patch('/update-data-with-request', async (request, response) => {
+    try {
+        const updatedPost = await Post.updateOne({
+            _id: request.body.Id
+        }, {
+            $set: { title: request.body.title }
+        }, {
+            $set: { description: request.body.description }
+        }, {
+            $set: { short: request.body.short }
+        });
+        response.json(updatedPost);
+    } catch (err) {
+        response.json({
+            mesaage: err
+        });
+    }
+});
+
+router.delete('/delete-data/:Id', async (request, response) => {
+    try {
+        const removedPost = await Post.remove({
+            _id: request.params.Id
+        });
+        response.json(removedPost);
+    } catch (err) {
+        response.json({
+            mesaage: err
+        });
+    }
+});
+
+router.delete('/delete-data-with-request', async (request, response) => {
+    try {
+        const removedPost = await Post.remove({
+            _id: request.body.Id
+        });
+        response.json(removedPost);
+    } catch (err) {
+        response.json({
+            mesaage: err
+        });
+    }
+});
+
+module.exports = router;
