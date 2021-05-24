237d93d28a641ac538cdc535a5ab1357c36df0c3
Turhan ŞİT <turhansit@gmail.com>
2021-05-24 10:56:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv/config');
 
 const app = express();
 app.use(bodyParser.json());
-app.use('/posts', postRoute)
+app.use('/api/v1', postRoute)
 
 // Connect to DB
 mongoose.connect(process.env.db_connect, {
